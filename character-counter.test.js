91b72dee607c2367be04b9f93be026225cd4375f
Christopher Ekfeldt <christopher.ekfeldt@valtech.com>
2020-02-10 14:27:20 +0100

    Changed config and fixed error in test

--- character-counter.test.js
+++ character-counter.test.js
@@ -3,6 +3,6 @@ const characterCounter = require('./character-counter');
 describe('countNonSlashCharacters()', function() {
   test('should return 4 when supplied "/page"', function() {
     const result = characterCounter.countNonSlashCharacters('/page');
-    expect(result).toBe(4);
+    expect(result).toBe(5);
   })
 })
